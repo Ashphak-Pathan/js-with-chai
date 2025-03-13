40385d98206d8d2cbcd3b21581bec829570b46de
Ashphak-Pathan <ashphakpathandev@gmail.com>
2025-03-13 01:32:14 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+/* alert( 3+ 3) We are using nodejs, Not a Browser */ 
+
+console.log( 3 + 3)
+
+console.log("Ashphak")
+
+let name = "Ashphak"
+let age = 18
+let isloggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  //object
