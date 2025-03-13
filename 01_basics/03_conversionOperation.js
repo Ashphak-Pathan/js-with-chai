b55b8b6cd1a9adbaba91a950b1b709231f389c64
Ashphak-Pathan <ashphakpathandev@gmail.com>
2025-03-13 09:39:51 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,23 @@ let someNumber = 33
 let stringNumber = String(someNumber) // is used to convert number to string
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//***************************Operations*********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2**3)
+console.log(2%2)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Ashphak" 
+let str3 = str1+str2
+console.log(str3);
